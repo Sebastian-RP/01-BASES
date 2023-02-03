8165c39943bfb269efdcb267292354d7f691b0ff
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2023-02-03 16:24:03 -0500

    task - module contador/contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Contadorcomponent } from './contador/contador.component';
+import { Contadorcomponent } from './contador/contador/contador.component';
 
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+//Tarea:
+//Crear un módulo llamado ContadorModule
+// declaraciones, y exportaciones para poderlo usar
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Contadorcomponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Contadorcomponent } from "./contador/contador.component";
+
+@NgModule({
+    declarations: [
+        Contadorcomponent
+    ],
+    exports: [
+        Contadorcomponent
+    ], 
+    imports: [
+        CommonModule
+    ]
+
+})
+export class ContadorModule {}
