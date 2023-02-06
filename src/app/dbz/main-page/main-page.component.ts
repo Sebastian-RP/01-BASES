99b2897597dc5640abfd55f40c4fbf2edf52ac82
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2023-02-05 22:09:04 -0500

    ngModule forms - not event prevenDefault

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -10,6 +15,25 @@ export class MainPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 140000
+  }
+
+  agregar(e: any) {
+    e.preventDefault(); //los formularios reactivos nos evitan el uso de esto
+    console.log("Hey!!!");
+  }
+
+  formularioReactivoAgregar() {
+    console.log("es una prueba");
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
   }
 
 }
